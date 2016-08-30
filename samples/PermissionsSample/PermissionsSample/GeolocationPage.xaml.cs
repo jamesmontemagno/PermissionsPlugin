9a3e8710980d16ca640ccb165cb9aa301d80e8fc
James Montemagno <james.montemagno@gmail.com>
2016-08-30 13:47:22 -0700

    Update sample code
    Update nugets and delete files that aren't needed.

--- samples/PermissionsSample/PermissionsSample/GeolocationPage.xaml.cs
+++ samples/PermissionsSample/PermissionsSample/GeolocationPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Geolocator.Plugin;
+using Plugin.Geolocator;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
